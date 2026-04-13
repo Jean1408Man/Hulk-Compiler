a8b05dbb266b785ac34107aa53ed676c8c31a7a0
melissa.sales <melissa.sales@aoshield.com>
2026-04-13 11:42:21 -0400

    feat: add span member to Decl and Expr classes for improved AST node representation

--- src/ast/abs_nodes/decl.h
+++ src/ast/abs_nodes/decl.h
@@ -2,11 +2,13 @@
 #define DECL_H
 
 #include "ast.h"
+#include "../../common/span.hpp"
 
 namespace Hulk {
 
     class Decl : public ASTnode {
     public:
+        hulk::common::Span span {};
         virtual ~Decl() = default;
     };
 
--- src/ast/abs_nodes/expr.h
+++ src/ast/abs_nodes/expr.h
@@ -2,11 +2,13 @@
 #define EXPR_H
 
 #include "ast.h"
+#include "../../common/span.hpp"
 
 namespace Hulk {
 
     class Expr : public ASTnode {
     public:
+        hulk::common::Span span {};
         virtual ~Expr() = default;
     };
 
